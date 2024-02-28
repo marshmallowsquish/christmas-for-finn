6a3b396d41c0f2b56dfd10be4dc76dec132b4829
Jade Kingsley <125752320+marshmallowsquish@users.noreply.github.com>
2024-02-28 16:30:02 -0600

    Make a function to create the calendar

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 /* DECLARE VARIABLES - selectors, working values */
 
+//selectors
+const contentContainer = document.getElementById("content-container");
+
 /* DECLARE FUNCTIONS */
 
 /* DECLARE EVENT HANDLERS */
 
 /* DECLARE NAMESPACES */
+const INIT = {
+  createCalendar: function() {
+    for (let i = 0; i < 25; i++) {
+      let ornamentBox = document.createElement("img");
+      ornamentBox.classList.add("ornament-box");
+      contentContainer.appendChild(ornamentBox);
+    }
+  }
+}
 
 /* SCRIPT */
+INIT.createCalendar();
