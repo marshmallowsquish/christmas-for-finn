fb016d3c9fb224f7f4b1016acbffe05212655899
Jade Kingsley <125752320+marshmallowsquish@users.noreply.github.com>
2024-05-04 13:16:42 -0500

    Personalize unlocked days

--- script.js
+++ script.js
@@ -16,8 +16,14 @@ const INIT = {
       ornamentBox.classList.add("ornament-box");
       contentContainer.appendChild(ornamentBox);
     }
+  },
+  addUnlockedDays: function() {
+    let days = document.getElementsByClassName("ornament-box");
+    days[0].setAttribute("src", "./img/1.png");
+    days[0].setAttribute("onclick", "location.href = './games/1/star-catcher.html';")
   }
 }
 
 /* SCRIPT */
 INIT.createCalendar();
+INIT.addUnlockedDays();
