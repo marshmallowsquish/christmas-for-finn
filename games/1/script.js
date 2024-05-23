1e6d8621df543b24db7ad6295551630d1a4b8114
Jade Kingsley <125752320+marshmallowsquish@users.noreply.github.com>
2024-05-23 12:54:36 -0500

    Create animations

--- games/1/script.js
+++ games/1/script.js
@@ -6,36 +6,39 @@ const soundsContainer = document.getElementById("sounds");
 const themesContainer = document.getElementById("themes");
 const bottomContainer = document.getElementById("bottom");
 const starCountDisplay = document.getElementById("stars");
-
 const star = document.getElementById("star");
 
 //working values
 let stars = 0;
 
 /* DECLARE FUNCTIONS */
-function startGame() {
-  setInterval(displayStar, getNumber(3000 + 1));
-}
 
 function displayStar() {
-  star.classList.remove("hidden");
-  star.animate(starKeyframes, starOptions);
+  star.animate(scaleTranslateStar, starOptions);
 }
 
 function getNumber(num) {
   return Math.floor(Math.random() * num);
 }
 
-const starKeyframes = [
-  { transform: "rotate(360deg) scale(0)"},
-  { transform: "rotate(0) scale(1)" },
-  { transform: "rotate(360deg) scale(0) translateX(100%)", backgroundColor: "white" },
-];
+function getPosOrNeg() {
+  let number = getNumber(2)
 
-const starOptions = {
-  duration: 1000,
-  iterations: 1,
-};
+  if (number === 0) {
+    return "-";
+  } else {
+    return "+";
+  }
+}
+
+function getXAxis() {
+  return `${getPosOrNeg()}${getNumber((centerDimensions.width / 2) + 1)}px`
+}
+
+function getYAxis() {
+  return `${getPosOrNeg()}${getNumber((centerDimensions.height / 2) + 1)}px`
+}
+
 
 /* DECLARE EVENT HANDLERS */
 backButton.addEventListener("click", function() {
@@ -81,4 +84,29 @@ INIT.createSoundButtons();
 INIT.createThemeButtons();
 INIT.createSongButtons();
 INIT.displayStarCount();
-startGame();
+
+//post-script selectors
+const centerDimensions = document.getElementById("center").getBoundingClientRect();
+
+//animations
+const scaleTranslateStar = [
+  { transform: `scale(0.${getNumber(9 + 1)}) translateX(${getXAxis()}) translateY(${getYAxis()})`, fill: "#FFFFFF"},
+  { transform: `scale(0.${getNumber(9 + 1)}) translateX(${getXAxis()}) translateY(${getYAxis()})`, fill: "#FFFFFF"},
+];
+
+const rotateScaleTranslateStar = [
+  { transform: `rotate(0) scale(0.${getNumber(9 + 1)}) translateX(${getXAxis()}) translateY(${getYAxis()}))`, fill: "#FFFFFF"},
+  { transform: `rotate(${getPosOrNeg()}360deg) scale(0.${getNumber(9 + 1)}) translateX(${getXAxis()}) translateY(${getYAxis()})`, fill: "#FFFFFF"},
+];
+
+const starOptions = {
+  duration: getNumber(6000 + 1) + 1000,
+  iterations: 1,
+  easing: `cubic-bezier(0.${getNumber(99 + 1)}, 0.${getNumber(99 + 1)}, 0.${getNumber(99 + 1)}, 0.${getNumber(99 + 1)})`
+};
+
+//game start
+setInterval(function() {
+  if (star.getAttribute("fill") === "#000000") displayStar();
+}, 1000)
+
